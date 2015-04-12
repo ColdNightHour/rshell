0bbcf6e65adbd4eae7cf0e575b6cbc53fb477332
John Dixon <jdixo001@hammer.cs.ucr.edu>
2015-04-12 01:01:33 -0700

    got hostname to work

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,18 @@
 #include <unistd.h>
 #include <pwd.h>
 #include <sys/types.h>
+#include <stdio.h>
+#include <errno.h>
+
 using namespace std;
 
 int main() {
         string userinput = "";
 	char hostname[64];
+	if(gethostname(hostname, sizeof hostname))
+		perror("failed to obtain hostname");
 	while(userinput != "exit") {
-		cout << getlogin() << "@" << gethostname(hostname, sizeof hostname) <<  "$ ";
+		cout << getlogin() << "@" << hostname <<  "$ ";
 		getline(cin, userinput);
 	}
 	return 0;
