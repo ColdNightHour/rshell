26de355c323310ad3de45db3d639d8fa773886b1
John Dixon <jdixo001@hammer.cs.ucr.edu>
2015-04-12 00:37:16 -0700

    began on the main part of the shell

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
-
+#include <unistd.h>
+#include <pwd.h>
+#include <sys/types.h>
 using namespace std;
 
 int main() {
+        string userinput = "";
+	while(userinput != "exit") {
+		getline(cin, userinput);
+	}
 	return 0;
 }
