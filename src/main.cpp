80aab23e7f868ec572b7e18c8c42cab20966a859
John Dixon <jdixo001@hammer.cs.ucr.edu>
2015-04-12 00:50:25 -0700

    tried the host name function

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 int main() {
         string userinput = "";
+	char hostname[64];
 	while(userinput != "exit") {
-		cout << getlogin() << "$";
+		cout << getlogin() << "@" << gethostname(hostname, sizeof hostname) <<  "$ ";
 		getline(cin, userinput);
 	}
 	return 0;
