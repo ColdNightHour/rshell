3eacb34e1a9cd86311d1926caee49dd53881942b
John Dixon <jdixo001@hammer.cs.ucr.edu>
2015-04-13 23:08:21 -0700

    began parse process, will add while later

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <iostream>
 #include <unistd.h>
 #include <pwd.h>
@@ -17,5 +18,10 @@ int main() {
 	char hostname[64];
 	if(gethostname(hostname, sizeof hostname))
 		perror("gethostname");
+	getline(cin, userinput);
+	char *command;
+	command = new char[userinput.size()];
+	strcpy(command, userinput.c_str());
+	cout << strlen(command);
 	return 0;
 }
