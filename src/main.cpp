3c5856e11d968a3972095373e1e8cfc5cba26e19
John Dixon <jdixo001@hammer.cs.ucr.edu>
2015-04-16 22:05:01 -0700

    little break, think I found an easier loop condition

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,6 @@
 using namespace std;
 
 void connectors(string userinput, vector<int> &x, vector<int> &y, bool &first) {
-	y.push_back(-2);
 	for(unsigned int i = 0; i < userinput.size() - 1; i++) {
 		if((userinput.at(i) == '|') && (userinput.at(i + 1) == '|')) {
 			x.push_back(0);
@@ -31,7 +30,7 @@ void connectors(string userinput, vector<int> &x, vector<int> &y, bool &first) {
 	}
 	if(userinput.at(0) == '&' || userinput.at(0) == '|' || userinput.at(0) == ';')
 		first = true;
-	y.push_back(-1);
+	x.push_back(-1);
 }
 
 int main() {
@@ -63,12 +62,12 @@ int main() {
 		unsigned int b = 0;
 		int y = 0;
 		char *arg[100000];
-		while(c_pos.at(y) != -1)  {
+		while(userinput.substr(x, c_pos.at(y) -x)) {
 			if(first) {
-				cout << "Error: file does not exist";
+				cout << "Error: file does not exist" << endl;
 				break;
 			}
-			if(c_pat.size() == 0)
+			if(c_pat.size() == 1)
 				strcpy(command, userinput.c_str());
 			else
 				strcpy(command, userinput.substr(x, c_pos.at(y) - x).c_str());
@@ -94,13 +93,13 @@ int main() {
 			unsigned int help = c_pat.at(y);
 			for(unsigned int i = 0; i < b; i++)
 				arg[i] = NULL;
-			if(help  == 0 && status != -1 && userinput.find("&&", y) != string::npos &&  userinput.find(";", y) != string::npos) 
+			if(help == 0 && status != -1 && userinput.find("&&", y) != string::npos && userinput.find(";", y) != string::npos) 
 				y++;
 			else if(help == 0 && status != -1) {
 				y++;
 				break;
 			}
-			else if(help == 1 && status == -1 && userinput.find("||", y) != string::npos &&  userinput.find(";", y) != string::npos) 
+			if(help == 1 && status == -1 && userinput.find("||", y) != string::npos && userinput.find(";", y) != string::npos) 
 				y++; 
 			else if(help == 1 && status == -1) {
 				y++;
@@ -110,6 +109,7 @@ int main() {
 				y++;
 			b = 0;			
 		}
+		
 	}	
 	return 0;
 }
