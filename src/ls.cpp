69a568b593cf7d823aa359a73a00bff029c9d279
John Dixon <jdixo001@hammer.cs.ucr.edu>
2015-04-25 18:51:15 -0700

    testing out dir operators

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <errno.h>
 
 using namespace std;
 
-int main() {
-	cout << "test";
+int main(int argc, char *argv[]) {
+	struct stat s;
+	DIR *current;
+	if(NULL == (current = opendir(argv[i]))) {
+		perror("Error in opening directory");
+		exit(1);
+	}
+	struct dirent *filespecs;
+	int result;
+	errno = 0;
+	while(result == (filespecs = readdir(current))) {
+		cout << filespecs->d_name << " ";
+	}
 	return 0;
 }
