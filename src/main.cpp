900a03d352717cd9379c9cfed95267b16427207d
John Dixon <jdixo001@hammer.cs.ucr.edu>
2015-05-05 12:53:36 -0700

    started to get ready for  redirect

--- src/main.cpp
+++ src/main.cpp
@@ -10,36 +10,8 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
-
+#include "main.h"
 using namespace std;
-
-void connectors(string userinput, vector<int> &x, vector<int> &y, bool &first, bool &multiple) {
-	x.clear();
-	y.clear();
-	for(unsigned int i = 0; i < userinput.size() - 1; i++) {
-		if((userinput.at(i) == '|') && (userinput.at(i + 1) == '|')) {
-			x.push_back(0);
-			y.push_back(i);
-		}
-		else if((userinput.at(i) == '&') && (userinput.at(i + 1) == '&')) {
-			x.push_back(1);
-			y.push_back(i);	
-		}
-		else if((userinput.at(i) == ';')) {
-			x.push_back(2);
-			y.push_back(i);	
-
-		}
-	}
-	y.push_back(userinput.size());
-	for(unsigned int i = 0; i < y.size() - 1; i++) {
-		if(y.at(i + 1) == y.at(i) + 1)
-			multiple = true;
-	} 
-	if(userinput.at(0) == '&' || userinput.at(0) == '|' || userinput.at(0) == ';')
-		first = true;
-	x.push_back(userinput.size());
-}
 
 int main() {
 	string userinput; 
